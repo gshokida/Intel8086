b87943b9d8a33e7a7a6fafee1013caf5133bf01f
gshokida <german.shokida@gmail.com>
2016-11-03 11:00:40 -0300

    Existencia funcionando

--- TP/existencia.asm
+++ TP/existencia.asm
@@ -35,7 +35,10 @@ segment datos data
 		ingresarCorrecto db 'Ha ingresado un elemento valido$'
 		ingresarIncorrecto db 'Debe ingresar un elemento valido 2 digitos alfanumericos$'
 		
-		mensajeConjuntoLleno db 'El conjunto ya está lleno$'
+		mensajeConjuntoLleno db 'El conjunto ya esta lleno$'
+		
+		mensajeExisteElemento db 'El elemento existe en el conjunto seleccionado$'
+		mensajeNoExisteElemento db 'El elemento no existe en el conjunto seleccionado$'
 		
 		numeroConjuntoChar resb 1
 		contadorConjunto db "1"
@@ -54,8 +57,7 @@ segment codigo code
 		mov sp,stacktop
 		
 inicio:
-
-		call  imprimirMenu 
+		call imprimirMenu 
 		
 		;opcion [1-5]
 		mov ah,1
@@ -63,9 +65,11 @@ inicio:
 
 		;obtengo la opcion ingresada		
 		mov byte[menuOpcion],al
-
-		cmp  byte[menuOpcion],'1'
-		je   ingresarElemento
+		
+		call ejecutarEnter
+		
+		cmp byte[menuOpcion],'1'
+		je ingresarElemento
 		
 		cmp  byte[menuOpcion],'2'
 		je   existenciaElemento
@@ -86,14 +90,225 @@ finPrograma:
 		mov ah,4ch
 		int 21h
 
+;**********************************************************************
+;Ingresar Elemento en un conjunto
+;**********************************************************************
+ingresarElemento:
+		call pedirIngresoConjunto
+		
+		call pedirIngresoElemento
+		
+		call agregarElemento
+		
+		call recorrerMatriz
+		
+		jmp inicio
+
+;**********************************************************************
+; Verificar existencia de elemento en conjunto
+;**********************************************************************
+existenciaElemento:
+		call pedirIngresoConjunto
+		
+		call pedirIngresoElemento
+		
+		call existeElemento
+		
+		cmp byte[flagElemento],"V"
+		je msgElementoExiste
+		
+		lea dx,[mensajeNoExisteElemento]
+		call imprimirMensaje
+		call ejecutarEnter
+		jmp finExistenciaElemento
+		
+msgElementoExiste:
+		lea dx,[mensajeExisteElemento]
+		call imprimirMensaje
+		call ejecutarEnter
+		
+finExistenciaElemento:
+		call recorrerMatriz
+		jmp inicio
 		
 ;**********************************************************************
-;*RUTINAS
+; Pido el ingreso del conjunto del 1 al 6 y valido
 ;**********************************************************************
+pedirIngresoConjunto:
+		lea dx,[mensajeIngresoConjunto]
+		call imprimirMensaje
+		call ejecutarEnter
+		
+		mov ah,1
+		int 21h
+		mov [numeroConjuntoChar],al
+		
+		call ejecutarEnter
+		
+		call validarIngresoConjunto
+		
+		cmp byte[conjuntoValido], "F"
+		je pedirIngresoConjunto
+		
+		ret
 
-;----------------------------------------------------------------------
-;  Menu general
-;----------------------------------------------------------------------
+validarIngresoConjunto:
+		mov byte[conjuntoValido], "V"
+		cmp byte[numeroConjuntoChar],"1"
+		jl ingresoConjuntoNoValido
+		cmp byte[numeroConjuntoChar],"6"
+		jg ingresoConjuntoNoValido
+finValidarIngresoConjunto:
+		ret
+		
+ingresoConjuntoNoValido:
+		mov byte[conjuntoValido], "F"
+		jmp finValidarIngresoConjunto
+
+
+;**********************************************************************
+; Pido el ingreso del elemento a agregar
+;**********************************************************************
+pedirIngresoElemento:
+		;pido el ingreso del elemento
+		lea dx,[mensajeIngresoElemento]
+		call imprimirMensaje
+		call ejecutarEnter
+		;leo el elemento ingresado
+		lea dx,[elemento-2]
+		mov ah,0ah
+		int 21h
+		;para finalizar la cadena con $
+		mov ax,0
+		mov al,[elemento-1]
+		mov si,ax
+		mov byte[elemento+si],'$'
+		
+		call validarElemento
+		
+		cmp  byte[flagElemento],"V"
+		je  elementoValido
+		lea dx,[ingresarIncorrecto]
+		call imprimirMensaje
+		call ejecutarEnter
+		jmp  pedirIngresoElemento
+elementoValido:
+		lea dx,[ingresarCorrecto]
+		call imprimirMensaje
+		call ejecutarEnter
+		ret
+		
+;**********************************************************************
+; Valido el elemento que quiere ingresar
+;**********************************************************************
+validarElemento:
+		mov si,0
+		mov dl,byte[elemento+si]
+		call validarDigitoElemento
+		cmp  byte[flagElemento],"F"
+		je  finValidarElemento
+		inc si
+		mov dl,byte[elemento+si]
+		call validarDigitoElemento
+finValidarElemento:
+		ret
+		
+validarDigitoElemento:
+		cmp dl,'0'
+		jl  noEsNumero
+		cmp dl,'9'
+		jg  noEsNumero
+		mov byte[flagElemento],"V"
+		jmp finValidarDigitoElemento
+noEsNumero:
+		cmp dl,'A'
+		jl  noEsLetra
+		cmp dl,'Z'
+		jg  noEsLetra
+		mov byte[flagElemento],"V"
+		jmp finValidarDigitoElemento
+noEsLetra:
+		mov byte[flagElemento],"F"
+finValidarDigitoElemento:
+		ret
+
+;**********************************************************************
+; Agrego el elemento al conjunto
+; uso registros si, bh, ax y dx
+;**********************************************************************
+agregarElemento:
+		mov bh,[contadorConjunto]
+		mov si,0
+siguienteAgregarConjunto:
+		cmp byte[numeroConjuntoChar],bh
+		jg avanzarAgregarConjunto
+		
+		mov byte[contador],0
+sigienteElementoAgregarConjunto:
+		inc byte[contador]
+		;Ver si es "  " si es igual grabo el elemento y salgo con finAgregarConjunto
+		cmp word[matriz+si],"  "
+		jne siguientePosicionAgregar
+		mov ax,[elemento]
+		mov word[matriz+si],ax
+		jmp finAgregarConjunto
+siguientePosicionAgregar:
+		add si,3
+		cmp byte[contador],20
+		jge conjuntoLleno
+		jmp sigienteElementoAgregarConjunto
+conjuntoLleno:
+		lea dx,[mensajeConjuntoLleno]
+		call imprimirMensaje
+		call ejecutarEnter
+finAgregarConjunto:
+		mov byte[contadorConjunto],"1"
+		ret
+		
+avanzarAgregarConjunto:
+		add si,60
+		add bh,1
+		jmp siguienteAgregarConjunto
+		
+;**********************************************************************
+; Verificar existencia de elemento en conjunto
+; Devuelve en flagElemento si existe o no
+; uso registros si, bh y ax
+;**********************************************************************	
+existeElemento:
+		mov byte[flagElemento],"F"
+		mov byte[contadorConjunto],"1"
+		mov ax,[elemento]
+		mov bh,[contadorConjunto]
+		mov si,0
+siguienteExisteElemento:
+		cmp byte[numeroConjuntoChar],bh
+		jg avanzarExisteElemento
+		mov byte[contador],0
+siguienteElementoExisteElemento:
+		inc byte[contador]
+		cmp word[matriz+si],"  "
+		je finExisteElemento
+		cmp word[matriz+si],ax
+		je existeElementoEnConjunto
+siguientePosicionExisteElemento:
+		add si,3
+		cmp byte[contador],20
+		jge finExisteElemento
+		jmp siguienteElementoExisteElemento
+finExisteElemento:		
+		mov byte[contadorConjunto],"1"
+		ret
+		
+avanzarExisteElemento:
+		add si,60
+		add bh,1
+		jmp siguienteExisteElemento
+
+existeElementoEnConjunto:
+		mov byte[flagElemento],"V"
+		jmp finExisteElemento
+		
 ;**********************************************************************
 ;Imprimir Menu
 ;**********************************************************************
@@ -131,7 +346,7 @@ imprimirMenu:
 		call ejecutarEnter
 
 		ret
-
+		
 ;**********************************************************************
 ;Imprimir opción no valida del menu
 ;**********************************************************************
@@ -140,223 +355,32 @@ imprimirMenuOpcionInvalida:
 		call imprimirMensaje
 		call ejecutarEnter
 		ret
-
-;----------------------------------------------------------------------
-;  FIN Menu general
-;----------------------------------------------------------------------
+		
+;**********************************************************************
+; Imprimo un enter en la pantalla
+;**********************************************************************		
+ejecutarEnter:
+		mov dx,salto
+		mov ah,9h
+		int 21h
+		ret
 
 ;**********************************************************************
-;Imprimir opción no valida del menu
+; Imprimo mensaje
+;**********************************************************************				
+imprimirMensaje:
+		mov ah,9
+		int 21h
+		ret
+
+
 ;**********************************************************************
-ingresarElemento:
-		call pedirIngresoConjunto
+; Eliminar Luego
+;**********************************************************************
 		
-		call pedirIngresoElemento
-		
-		call agregarElemento
-		
-		call recorrerMatriz
-		
-		ret
-
-
-;----------------------------------------------------------------------
-; Lleno la matriz con otros valores
-;----------------------------------------------------------------------
-llenarMatriz:
-		mov bx,0
-		mov ax,[elemento]
-		mov word[matriz+bx],ax
-		
-		mov bx,63
-		mov word[matriz+bx],ax
-		
-		mov bx,126
-		mov word[matriz+bx],ax
-		
-		mov bx,189
-		mov word[matriz+bx],ax
-		
-		mov bx,252
-		mov word[matriz+bx],ax
-		
-		mov bx,315
-		mov word[matriz+bx],ax
-		
-		ret
-
-;----------------------------------------------------------------------
-; Pido el ingreso del conjunto del 1 al 6 y valido
-;----------------------------------------------------------------------
-pedirIngresoConjunto:
-		lea dx,[mensajeIngresoConjunto]
-		call imprimirMensaje
-		
-		mov ah,1
-		int 21h
-		mov [numeroConjuntoChar],al
-		
-		call ejecutarEnter
-		
-		call validarIngresoConjunto
-		
-		cmp byte[conjuntoValido], "F"
-		je pedirIngresoConjunto
-		
-		ret
-
-validarIngresoConjunto:
-		mov byte[conjuntoValido], "V"
-		cmp byte[numeroConjuntoChar],"1"
-		jl ingresoConjuntoNoValido
-		cmp byte[numeroConjuntoChar],"6"
-		jg ingresoConjuntoNoValido
-finValidarIngresoConjunto:
-		ret
-		
-ingresoConjuntoNoValido:
-		mov byte[conjuntoValido], "F"
-		jmp finValidarIngresoConjunto
-
-
-;----------------------------------------------------------------------
-; Pido el ingreso del elemento a agregar
-;----------------------------------------------------------------------
-pedirIngresoElemento:
-		;pido el ingreso del elemento
-		lea dx,[mensajeIngresoElemento]
-		call imprimirMensaje
-		call ejecutarEnter
-		;leo el elemento ingresado
-		lea dx,[elemento-2]
-		mov ah,0ah
-		int 21h
-		;para finalizar la cadena con $
-		mov ax,0
-		mov al,[elemento-1]
-		mov si,ax
-		mov byte[elemento+si],'$'
-		
-		call validarElemento
-		
-		cmp  byte[flagElemento],"V"
-		je  elementoValido
-		lea dx,[ingresarIncorrecto]
-		call imprimirMensaje
-		call ejecutarEnter
-		jmp  pedirIngresoElemento
-elementoValido:
-		lea dx,[ingresarCorrecto]
-		call imprimirMensaje
-		call ejecutarEnter
-		ret
-		
-;----------------------------------------------------------------------
-; Valido el elemento que quiere ingresar
-;----------------------------------------------------------------------
-validarElemento:
-		mov si,0
-		mov dl,byte[elemento+si]
-		call validarDigitoElemento
-		cmp  byte[flagElemento],"F"
-		je  finValidarElemento
-		inc si
-		mov dl,byte[elemento+si]
-		call validarDigitoElemento
-finValidarElemento:
-		ret
-		
-validarDigitoElemento:
-		cmp dl,'0'
-		jl  noEsNumero
-		cmp dl,'9'
-		jg  noEsNumero
-		mov byte[flagElemento],"V"
-		jmp finValidarDigitoElemento
-noEsNumero:
-		cmp dl,'A'
-		jl  noEsLetra
-		cmp dl,'Z'
-		jg  noEsLetra
-		mov byte[flagElemento],"V"
-		jmp finValidarDigitoElemento
-noEsLetra:
-		mov byte[flagElemento],"F"
-finValidarDigitoElemento:
-		ret
-
-;----------------------------------------------------------------------
-; Agrego el elemento al conjunto
-;----------------------------------------------------------------------
-agregarElemento:
-		mov bh,[contadorConjunto]
-		mov si,0
-siguienteAgregarConjunto:
-		cmp byte[numeroConjuntoChar],bh
-		jg avanzarAgregarConjunto
-		
-		mov byte[contador],0
-sigienteElementoAgregarConjunto:
-		inc byte[contador]
-		;Ver si es "  " si es igual grabo el elemento y salgo con finAgregarConjunto
-		cmp word[matriz+si],"  "
-		jne siguientePosicionAgregar
-		mov ax,[elemento]
-		mov word[matriz+si],ax
-		jmp finAgregarConjunto
-siguientePosicionAgregar:
-		add si,3
-		cmp byte[contador],20
-		jge conjuntoLleno
-		jmp sigienteElementoAgregarConjunto
-conjuntoLleno:
-		lea dx,[mensajeConjuntoLleno]
-		call imprimirMensaje
-		call ejecutarEnter
-finAgregarConjunto:
-		mov byte[contadorConjunto],"1"
-		ret
-		
-avanzarAgregarConjunto:
-		add si,60
-		add bh,1
-		jmp siguienteAgregarConjunto
-
-agregoElementoEnPosicion:
-		
-		
-;----------------------------------------------------------------------
-; Muestro el conjunto que elijo
-;----------------------------------------------------------------------
-mostrarConjunto:
-		mov bh,[contadorConjunto]
-		mov si,0
-siguienteConjunto:
-		cmp byte[numeroConjuntoChar],bh
-		jg avanzarConjunto
-		
-		mov byte[contador],0
-sigienteElementoConjunto:
-		inc byte[contador]
-		lea dx,[matriz+si]
-		call imprimirMensaje
-		add si,3
-		cmp byte[contador],20
-		jge finMostrarConjunto
-		jmp sigienteElementoConjunto
-finMostrarConjunto:
-		call ejecutarEnter
-		mov byte[contadorConjunto],"1"
-		ret
-		
-avanzarConjunto:
-		add si,60
-		add bh,1
-		jmp siguienteConjunto
-
-;----------------------------------------------------------------------
+;**********************************************************************
 ; Recorro la matriz y la muestro
-;----------------------------------------------------------------------
+;**********************************************************************
 recorrerMatriz:
 		mov bx,0
 		mov si,0
@@ -379,32 +403,3 @@ imprimirFinColumna:
 		mov byte[contador],0
 		jmp vuelvoElemento
 
-;----------------------------------------------------------------------
-; Verificar existencia de elemento en conjunto
-;----------------------------------------------------------------------
-existenciaElemento:
-		call pedirIngresoConjunto
-		
-		call pedirIngresoElemento
-		
-		;call existeElemento
-		
-		call recorrerMatriz
-		ret
-		
-;----------------------------------------------------------------------
-; Imprimo un enter en la pantalla
-;----------------------------------------------------------------------		
-ejecutarEnter:
-		mov dx,salto
-		mov ah,9h
-		int 21h
-		ret
-
-;----------------------------------------------------------------------
-; Imprimo mensaje
-;----------------------------------------------------------------------				
-imprimirMensaje:
-		mov ah,9
-		int 21h
-		ret
